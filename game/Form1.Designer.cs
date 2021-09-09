995dd0c0681d98400a51fba88396dd9053494795
rdd0582 <72299861+rdd0582@users.noreply.github.com>
2021-09-09 20:32:48 +0100

    Add files via upload

---
+++ game/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.characterPb = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.characterPb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // characterPb
+            // 
+            this.characterPb.BackColor = System.Drawing.Color.Transparent;
+            this.characterPb.Image = global::game.Properties.Resources.steve;
+            this.characterPb.Location = new System.Drawing.Point(272, 212);
+            this.characterPb.Name = "characterPb";
+            this.characterPb.Size = new System.Drawing.Size(126, 70);
+            this.characterPb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.characterPb.TabIndex = 0;
+            this.characterPb.TabStop = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 16;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.characterPb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.characterPb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox characterPb;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ game/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Media;
+
+namespace game
+{
+    public partial class Form1 : Form
+    {
+        public const int speed = 10;
+        public bool left = false;
+        public bool right = false;
+        public bool up = false;
+        public bool down = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                left = true;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                right = true;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                up = true;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                down = true;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                left = false;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                right = false;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                up = false;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                down = false;
+            }
+        }
+
+        private void PlayerMove()
+        {
+            if (left | right | up | down)
+            {
+                SoundPlayer sound = new SoundPlayer(@"C:\Users\Rupert\Documents\College\Computer Science\game\Resources\haha.wav");
+                sound.Play();
+            }
+
+            if (left)
+            {
+                int x = characterPb.Location.X;
+                int y = characterPb.Location.Y;
+                int moveSpeed = speed;
+                characterPb.Location = new Point(x - moveSpeed, y);
+            }
+            if (right)
+            {
+                int x = characterPb.Location.X;
+                int y = characterPb.Location.Y;
+                int moveSpeed = speed;
+                characterPb.Location = new Point(x + moveSpeed, y);
+            }
+            if (up)
+            {
+                int x = characterPb.Location.X;
+                int y = characterPb.Location.Y;
+                int moveSpeed = speed;
+                characterPb.Location = new Point(x, y - moveSpeed);
+            }
+            if (down)
+            {
+                int x = characterPb.Location.X;
+                int y = characterPb.Location.Y;
+                int moveSpeed = speed;
+                characterPb.Location = new Point(x, y + moveSpeed);
+            }
+        }
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            PlayerMove();
+        }
+    }
+}
